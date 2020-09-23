8f4b33bd609b75cad03316724bc93f612966045d
Teke <fatih.avila@gmail.com>
2020-09-23 17:32:02 +0700

    fix video material

--- Digilearn_001/Task/Course/Action/MaterialVideoViewController.swift
+++ Digilearn_001/Task/Course/Action/MaterialVideoViewController.swift
@@ -17,10 +17,12 @@ class MaterialVideoViewController: BaseActionViewController, ActionDelegate {
     var quiz: AssessmentQuizModel?
     var index: Int?
     @IBOutlet weak var cardView: MDCCard!
+    @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var prevButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var materialTitle: UILabel!
+    @IBOutlet weak var videoPlayerBackgroundView: UIView!
     
     @IBOutlet weak var videoPlayer: ASPVideoPlayer!
     
@@ -49,6 +51,10 @@ class MaterialVideoViewController: BaseActionViewController, ActionDelegate {
         
         videoPlayer.videoAssets = [secondAsset]
         videoPlayer.videoPlayerControls.tintColor = UIColor(named: "red_1")
+        
+        videoPlayer.resizeClosure = { [unowned self] isExpanded in
+            self.rotate(isExpanded: isExpanded)
+        }
     
     }
     
@@ -63,5 +69,105 @@ class MaterialVideoViewController: BaseActionViewController, ActionDelegate {
     
     func onSubmitProgress(message: String) {
         
+    }
+    
+    var previousConstraints: [NSLayoutConstraint] = []
+    
+    func rotate(isExpanded: Bool) {
+        let views: [String:Any] = ["videoPlayer": videoPlayer as Any,
+                                   "backgroundView": videoPlayerBackgroundView as Any]
+
+        var constraints: [NSLayoutConstraint] = []
+
+        if isExpanded == false {
+            self.containerView.removeConstraints(self.videoPlayer.constraints)
+
+            self.view.addSubview(self.videoPlayerBackgroundView)
+            self.view.addSubview(self.videoPlayer)
+            self.videoPlayer.frame = self.containerView.frame
+            self.videoPlayerBackgroundView.frame = self.containerView.frame
+
+            let padding = (self.view.bounds.height - self.view.bounds.width) / 2.0
+
+            var bottomPadding: CGFloat = 0
+
+            if #available(iOS 11.0, *) {
+                if self.view.safeAreaInsets != .zero {
+                    bottomPadding = self.view.safeAreaInsets.bottom
+                }
+            }
+
+            let metrics: [String:Any] = ["padding":padding,
+                                         "negativePaddingAdjusted":-(padding - bottomPadding),
+                                         "negativePadding":-padding]
+
+            constraints.append(contentsOf:
+                NSLayoutConstraint.constraints(withVisualFormat: "H:|-(negativePaddingAdjusted)-[videoPlayer]-(negativePaddingAdjusted)-|",
+                                               options: [],
+                                               metrics: metrics,
+                                               views: views))
+            constraints.append(contentsOf:
+                NSLayoutConstraint.constraints(withVisualFormat: "V:|-(padding)-[videoPlayer]-(padding)-|",
+                                               options: [],
+                                               metrics: metrics,
+                                               views: views))
+
+            constraints.append(contentsOf:
+                NSLayoutConstraint.constraints(withVisualFormat: "H:|-(negativePadding)-[backgroundView]-(negativePadding)-|",
+                                               options: [],
+                                               metrics: metrics,
+                                               views: views))
+            constraints.append(contentsOf:
+                NSLayoutConstraint.constraints(withVisualFormat: "V:|-(padding)-[backgroundView]-(padding)-|",
+                                               options: [],
+                                               metrics: metrics,
+                                               views: views))
+
+            self.view.addConstraints(constraints)
+        } else {
+            self.view.removeConstraints(self.previousConstraints)
+
+            let targetVideoPlayerFrame = self.view.convert(self.videoPlayer.frame, to: self.containerView)
+            let targetVideoPlayerBackgroundViewFrame = self.view.convert(self.videoPlayerBackgroundView.frame, to: self.containerView)
+
+            self.containerView.addSubview(self.videoPlayerBackgroundView)
+            self.containerView.addSubview(self.videoPlayer)
+
+            self.videoPlayer.frame = targetVideoPlayerFrame
+            self.videoPlayerBackgroundView.frame = targetVideoPlayerBackgroundViewFrame
+
+            constraints.append(contentsOf:
+                NSLayoutConstraint.constraints(withVisualFormat: "H:|[videoPlayer]|",
+                                               options: [],
+                                               metrics: nil,
+                                               views: views))
+            constraints.append(contentsOf:
+                NSLayoutConstraint.constraints(withVisualFormat: "V:|[videoPlayer]|",
+                                               options: [],
+                                               metrics: nil,
+                                               views: views))
+
+            constraints.append(contentsOf:
+                NSLayoutConstraint.constraints(withVisualFormat: "H:|[backgroundView]|",
+                                               options: [],
+                                               metrics: nil,
+                                               views: views))
+            constraints.append(contentsOf:
+                NSLayoutConstraint.constraints(withVisualFormat: "V:|[backgroundView]|",
+                                               options: [],
+                                               metrics: nil,
+                                               views: views))
+
+            self.containerView.addConstraints(constraints)
+        }
+
+        self.previousConstraints = constraints
+        
+        UIView.animate(withDuration: 0.25, delay: 0.0, options: [], animations: {
+            self.videoPlayer.transform = isExpanded == true ? .identity : CGAffineTransform(rotationAngle: .pi / 2.0)
+            self.videoPlayerBackgroundView.transform = isExpanded == true ? .identity : CGAffineTransform(rotationAngle: .pi / 2.0)
+
+            self.view.layoutIfNeeded()
+        })
     }
 }
