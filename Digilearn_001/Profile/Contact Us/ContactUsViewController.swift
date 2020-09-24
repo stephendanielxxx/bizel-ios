10ed6df71e1b4249b99e9ef51fbb767219690a95
Teke <fatih.avila@gmail.com>
2020-09-24 13:46:39 +0700

    send email function

--- Digilearn_001/Profile/Contact Us/ContactUsViewController.swift
+++ Digilearn_001/Profile/Contact Us/ContactUsViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MessageUI
 
-class ContactUsViewController: UIViewController {
+class ContactUsViewController: UIViewController, MFMailComposeViewControllerDelegate {
 
     @IBOutlet weak var message: UITextView!
     @IBOutlet weak var buttonSend: UIButton!
@@ -24,5 +25,34 @@ class ContactUsViewController: UIViewController {
 
     @IBAction func backAction(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func sendEmailAction(_ sender: UIButton) {
+        if message.text.isEmpty {
+            let alert = UIAlertController(title: "Contact Us", message: "Message must be filled", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        }else{
+            sendEmail(message: message.text)
+        }
+    }
+    
+    func sendEmail(message: String) {
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            mail.setToRecipients(["stephendaniel60@gmail.com"])
+            mail.setMessageBody(message, isHTML: true)
+
+            present(mail, animated: true)
+        } else {
+            let alert = UIAlertController(title: "Contact Us", message: "Please set up your email first", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        }
+    }
+
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true)
     }
 }
