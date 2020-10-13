3017a6754d5662a77468f0787625b8240b666c74
Stephen <stephendanielxxx@gmail.com>
2020-10-13 10:39:53 +0700

    Change quiz action to uitapgesture

---
+++ Digilearn_001/Task/Course/Action/Custom/QuizAnswerTapGesture.swift
@@ -0,0 +1,14 @@
+//
+//  QuizAnswerTapGesture.swift
+//  Digilearn_001
+//
+//  Created by Teke on 13/10/20.
+//  Copyright © 2020 Digimaster. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class QuizAnswerTapGesture: UITapGestureRecognizer {
+    var answer: String!
+}
--- Digilearn_001/Task/Course/Action/QuizViewController.swift
+++ Digilearn_001/Task/Course/Action/QuizViewController.swift
@@ -80,19 +80,28 @@ class QuizViewController: BaseActionViewController, ActionDelegate {
         buttonB.answer = quiz?.pil2
         buttonC.answer = quiz?.pil3
         buttonD.answer = quiz?.pil4
+
+        let tap = QuizAnswerTapGesture(target: self, action: #selector(checkAnswerA(_:)))
+        tap.answer = quiz?.pil1
+        buttonA.addGestureRecognizer(tap)
         
-        buttonA.addTarget(self, action: #selector(QuizViewController.checkAnswerA(_:)), for: .touchUpInside)
+        let tapB = QuizAnswerTapGesture(target: self, action: #selector(checkAnswerB(_:)))
+        tapB.answer = quiz?.pil2
+        buttonA.addGestureRecognizer(tapB)
         
-        buttonB.addTarget(self, action: #selector(QuizViewController.checkAnswerB(_:)), for: .touchUpInside)
+        let tapC = QuizAnswerTapGesture(target: self, action: #selector(checkAnswerC(_:)))
+        tapC.answer = quiz?.pil3
+        buttonA.addGestureRecognizer(tapC)
         
-        buttonC.addTarget(self, action: #selector(QuizViewController.checkAnswerC(_:)), for: .touchUpInside)
+        let tapD = QuizAnswerTapGesture(target: self, action: #selector(checkAnswerD(_:)))
+        tapD.answer = quiz?.pil4
+        buttonA.addGestureRecognizer(tapD)
         
-        buttonD.addTarget(self, action: #selector(QuizViewController.checkAnswerD(_:)), for: .touchUpInside)
         
         scrollview.bounces = (scrollview.contentOffset.y > 100);
     }
     
-    @objc func checkAnswerA(_ sender: CustomChoiceCardView?) {
+    @objc func checkAnswerA(_ sender: QuizAnswerTapGesture?) {
         lineA.backgroundColor = UIColor(named: "color_EFB8CB")
         lineB.backgroundColor = UIColor.darkGray
         lineC.backgroundColor = UIColor.darkGray
@@ -107,7 +116,7 @@ class QuizViewController: BaseActionViewController, ActionDelegate {
         
     }
     
-    @objc func checkAnswerB(_ sender: CustomChoiceCardView?) {
+    @objc func checkAnswerB(_ sender: QuizAnswerTapGesture?) {
         lineA.backgroundColor = UIColor.darkGray
         lineB.backgroundColor = UIColor(named: "color_EFB8CB")
         lineC.backgroundColor = UIColor.darkGray
@@ -121,7 +130,7 @@ class QuizViewController: BaseActionViewController, ActionDelegate {
         }
     }
     
-    @objc func checkAnswerC(_ sender: CustomChoiceCardView?) {
+    @objc func checkAnswerC(_ sender: QuizAnswerTapGesture?) {
         lineA.backgroundColor = UIColor.darkGray
         lineB.backgroundColor = UIColor.darkGray
         lineC.backgroundColor = UIColor(named: "color_EFB8CB")
@@ -135,8 +144,7 @@ class QuizViewController: BaseActionViewController, ActionDelegate {
         }
     }
     
-    @objc func checkAnswerD(_ sender: CustomChoiceCardView?) {
-    
+    @objc func checkAnswerD(_ sender: QuizAnswerTapGesture?) {
         lineA.backgroundColor = UIColor.darkGray
         lineB.backgroundColor = UIColor.darkGray
         lineC.backgroundColor = UIColor.darkGray
